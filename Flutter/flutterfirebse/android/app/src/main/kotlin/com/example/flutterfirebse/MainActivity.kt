5b2c2b2d8008aa18e2e84df09c69002dd25b8190
lofi0boy@gmail.com <lofi0boy@gmail.com>
2022-10-16 17:22:35 +0900

    flutter-python 연동

---
+++ Flutter/flutterfirebse/android/app/src/main/kotlin/com/example/flutterfirebse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterfirebse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Python/app.py
+++ Python/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import json
-import sys
 import Board
+import sys
 
 app = Flask(__name__)
 
@@ -12,14 +12,17 @@ def hello_world():
 @app.route("/tellMeWhatTodo",methods=['POST'])
 def announce(): #lastStateJson : turn 1 or 2,lastPosition [x,y],board,withAI 0or1
 
-    game = Board.Board()
     inputDic = json.loads(request.get_data())
+
+
+    game = Board.GomokuReferee()
     withAI = True if inputDic["withAI"] == 1 else False
     game.board = inputDic["board"]
-    game.color = inputDic["turn"]
     lastPosition = inputDic["lastPosition"]
-    lastY = lastPosition[0]
-    lastX = lastPosition[1]
+
+    currentTurn = inputDic["turn"]
+    # lastY = lastPosition[0]
+    # lastX = lastPosition[1]
 
     answerX =-1
     answerY =-1
@@ -27,14 +30,15 @@ def announce(): #lastStateJson : turn 1 or 2,lastPosition [x,y],board,withAI 0or
     returnDic={"winner":0,"answerPosition":[answerX,answerY]}
 
     if game.is_win(lastPosition[0],lastPosition[1]) == 1 : #사용자가 이겼는지 체크하는 함수
-        returnDic["winner"] = game.color;
+        returnDic["winner"] = currentTurn;
         return json.dumps(returnDic)
     elif game.is_win(lastPosition[0],lastPosition[1]) == -1 :
-        if(game.color ==1):
+        if(currentTurn==1):
             returnDic["winner"] = 2
         else:
             returnDic["winner"] = 1
         return json.dumps(returnDic)
+
     #승자가 없다면
     if(withAI): # 여긴 나중에 선우 알고리즘 합침
         answerX = 1
