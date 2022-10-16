2a05ca6eab6a216cd311e6adc2d058357f0f2f2d
lofi0boy@gmail.com <lofi0boy@gmail.com>
2022-10-16 19:24:48 +0900

    no message

--- Python/app.py
+++ Python/app.py
@@ -48,4 +48,5 @@ def announce(): #lastStateJson : turn 1 or 2,lastPosition [x,y],board,withAI 0or
     #returnDic : winner 0or1or2, answerPosition : [x,y]
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=False)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
